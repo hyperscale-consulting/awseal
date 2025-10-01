bf4e1f55c063bd13e7451b68b1221f94b20bb969
Andy Caine <andy@hyperscale.consulting>
2025-10-01 20:16:51 +0100

    chore: version bump

--- Sources/Awseal.swift
+++ Sources/Awseal.swift
@@ -515,7 +515,7 @@ func printRoleCredentials(creds: RoleCreds) {
 struct Awseal: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "An AWS CLI credential_process using AWS SSO to mint credentials while storing secrets under a Secure Enclave key.",
-        version: "0.3.0",
+        version: "0.3.1",
         subcommands: [Login.self, FetchRoleCreds.self]
     )
 }
